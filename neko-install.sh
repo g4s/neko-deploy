1f84199c843d2069b1b762fcf35bf6c63daa9ecd
Gregor Segner <gregor.segner@gmail.com>
2025-07-24 18:11:00 +0200

    Update neko-install.sh

--- neko-install.sh
+++ neko-install.sh
@@ -6,13 +6,15 @@ if [[ $(id -u) == 0 ]];
         ENV_FILE="/etc/sysconfig/neko"
 
         podman pull "${NEKO_IMAGE}"
-        podman run --replace --privileged \
+        podman run --replace --privileged -d \
             --add-cap=SYS_ADMIN \
             --snm-size=2g \
             --label=app=neko \
             --label=dev.dozzle.group=neko \
             --label=tsdproxy.enable=tru \
             --env-file "${ENV_FILE}" \
+            -e NEKO_WEBRTC_EPR= \
+            -e NEKO_WEBRTC_NAT1TO1= \
             --name neko \
             "${NEKO_IMAGE}"
     else
